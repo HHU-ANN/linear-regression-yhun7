2a82ba0fcb2a0776c5c60809cdac0e7b9974b4f0
yhun7 <1261204577@qq.com>
2023-05-01 23:21:53 +0800

    2 commit

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,7 +8,7 @@ except ImportError as e:
     import numpy as np
 
 
-def ridge(data, alpha=0.1):
+def ridge(data, alpha=0.01):
     x, y = read_data()
     n_features = x.shape[1]
     a = np.eye(n_features)
