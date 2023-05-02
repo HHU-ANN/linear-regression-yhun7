bb418ea751e5a35de9622db917a7c8fb6f753644
yhun7 <1261204577@qq.com>
2023-05-03 00:47:34 +0800

    17 commit

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -12,7 +12,7 @@ def ridge(data, alpha=0.01):
     x, y = read_data()
     n_features = x.shape[1]
     a = np.eye(n_features)
-    weight = np.dot(np.linalg.inv(np.dot(x.T, x) + alpha * a),  np.dot(x.T, y)
+    weight = np.dot(np.linalg.inv(np.dot(x.T, x) + alpha * a),  np.dot(x.T, y))
     return data @ weight
 
 def lasso(data, alpha=0.01, lr=1e-13, max_iter=150000):
