3b5fca228828f9391e9edffe6fbe7b48e9c171fe
yhun7 <1261204577@qq.com>
2023-05-03 00:58:59 +0800

    177 commit

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,14 +8,14 @@ except ImportError as e:
     import numpy as np
 
 
-def ridge(data, alpha=0.01):
+def ridge(data, alpha=0):
     x, y = read_data()
     n_features = x.shape[1]
     a = np.eye(n_features)
     weight = np.dot(np.linalg.inv(np.dot(x.T, x) + alpha * a),  np.dot(x.T, y))
     return data @ weight
 
-def lasso(data, alpha=0.01, lr=1e-13, max_iter=150000):
+def lasso(data, alpha=0.01, lr=1e-15, max_iter=150000):
     x, y = read_data()
     n_sample, n_features = x.shape
     weight = np.zeros((n_features, 1))
