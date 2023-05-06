c8b6c0d98bac61849c56a68ec0db06673c3bb4d2
yhun7 <1261204577@qq.com>
2023-05-06 23:49:00 +0800

    11

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -14,16 +14,15 @@ def ridge(data, alpha=-0.1):
     weight = np.dot(np.linalg.inv(np.dot(x.T, x) + alpha * a),  np.dot(x.T, y))
     return data @ weight
 
-def lasso(data, alpha=2800, lr=1e-10, max_iter=100000):
+def lasso(data, alpha=2800, lr=9e-10, max_iter=2e5):
     x, y = read_data()
     weight = data
-    y_pred = np.dot(weight, x.T)
     for i in range(max_iter):
         y_pred = np.dot(weight, x.T)
         error = y_pred - y
         gradient = np.dot(error, x) + alpha * np.sign(weight)
         weight = weight * (1 - (lr * alpha / 6)) - gradient * lr
-    return data @ weight
+    return weight @ data
 
 
 def read_data(path='./data/exp02/'):
