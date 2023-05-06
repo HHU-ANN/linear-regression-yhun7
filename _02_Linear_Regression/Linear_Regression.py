684778f5da7f6af464fcb8fe0c4b68c436acc547
yhun7 <1261204577@qq.com>
2023-05-06 22:55:39 +0800

    1 commit

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -6,32 +6,23 @@ try:
 except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
-def add_polynomial_features(X, degree=2):
-    n_sample, n_feature = X.shape
-    X_poly = np.ones((n_sample, 1))
-    for d in range(1, degree + 1):
-        for j in range(n_feature):
-            X_poly = np.concatenate((X_poly, np.power(X[:, j:j+1], d)), axis=1)
-    X_poly = X_poly[:, :n_feature]
-    return X_poly
 
-def ridge(data, alpha=1, degree=2):
+
+def ridge(data, alpha=1):
     x, y = read_data()
-    x_poly = add_polynomial_features(x, degree=degree)
-    n_features = x_poly.shape[1]
+    n_features = x.shape[1]
     a = np.eye(n_features)
-    weight = np.dot(np.linalg.inv(np.dot(x_poly.T, x) + alpha * a),  np.dot(x_poly.T, y))
+    weight = np.dot(np.linalg.inv(np.dot(x.T, x) + alpha * a),  np.dot(x.T, y))
     return data @ weight
 
 def lasso(data, alpha=0.1, lr=1e-12, max_iter=150000):
     x, y = read_data()
-    n_sample, n_features = x.shape
-    weight = np.zeros((n_features, 1))
+    weight = data
     for i in range(max_iter):
-        y_pred = x @ weight
-        error = y - y_pred
-        gradient = -2 * (x.T @ error) + alpha * np.sign(weight)
-        weight -= lr * gradient.mean(axis=1, keepdims=True)
+        y_pred =  np.dot(weight, x.T)
+        error = y_pred - y
+        gradient = np.dot(error, x) + alpha * np.sign(weight)
+        weight = weight * (1 - (lr * alpha / 6)) - gradient * weight
     return data @ weight
 
 
